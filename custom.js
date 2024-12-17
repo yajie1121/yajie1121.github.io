86644f689a1ae2e9dc230bae008afdae8000b6b5
yajie1121 <13146960716@163.com>
2024-12-17 10:11:18 +0800

    压缩图片

--- custom.js
+++ custom.js
@@ -1,39 +1,20 @@
-// 霸都丶傲天 2019.10.10
 $(function () {
-    let currentIndex = 0;
-    const container = $("#texts-container");
-    const intervalTime = 500; // 每1秒切换一次
-
-    function showNextText() {
-        if (currentIndex >= config.texts.length) {
-            currentIndex = 0;
-        }
-
-        const item = config.texts[currentIndex];
-        const p = document.createElement("p");
+    let dom = document.createElement("span");
+    config.texts.forEach(function (item) {
+        let p = document.createElement("p");
         p.innerHTML = item;
-
         if (config.imgs && config.imgs[item]) {
-            const img = document.createElement("img");
-            img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"; // 占位符图片
-            img.setAttribute("data-src", config.imgs[item]);
+            let img = document.createElement("img");
+            img.src = config.imgs[item];
             img.setAttribute("class", 'text-img');
             p.appendChild(img);
-
-            // 确保图片加载
-            img.onload = function () {
-                img.src = img.getAttribute('data-src');
-            };
         }
+        dom.appendChild(p);
+    });
+    $("#texts-container").append(dom.innerHTML);
+});
 
-        container.empty().append(p);
-        currentIndex++;
-    }
-
-    // 启动定时器
-    setInterval(showNextText, intervalTime);
-
-    // 设置描述内容
+$(function () {
     for (let k in config.desc) {
         let dom = $("#" + k);
         if (dom.length > 0 && config.desc[k]) {
